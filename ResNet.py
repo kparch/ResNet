70bbf962de7aff68c8a83c93aa125085e624aa1c
KasiaPar <kathrinp@online.de>
2021-04-05 11:25:53 +0200

    basic architecture without batchnorm etc

--- ResNet.py
+++ ResNet.py
@@ -16,21 +16,16 @@ class ResNet(nn.Module):
         super().__init__()
         self.n = n
         self.initial_layer = nn.Conv2d(3, 16, kernel_size=3, padding=1)
-        self.layerBlock1 = ResidualBlocks(16, n)
-        self.layerBlock2 = ResidualBlocks(16, n)
-        self.layerBlock3 = ResidualBlocks(16, n)
-        self.avgpool = nn.AvgPool2d(kernel_size=16)
+        self.layerBlock1 = ResidualBlocks(16, 16, n)
+        self.layerBlock2 = ResidualBlocks(16, 32, n)
+        self.layerBlock3 = ResidualBlocks(32, 64, n)
+        self.avgpool = nn.AvgPool2d(kernel_size=8)
         self.fc = nn.Linear(64, 10)
         
     def forward(self, x):
-        # Without subsampling
-        print('Check0')
         initial_layer = self.initial_layer(x)
-        print('Check1')
         resblocks_1 = self.layerBlock1(initial_layer)
-        print('Check2')
         resblocks_2 = self.layerBlock2(resblocks_1)
-        print('Check3')
         resblocks_3 = self.layerBlock3(resblocks_2)
         avg_pool = self.avgpool(resblocks_3)
         flat = flatten(avg_pool)
@@ -46,16 +41,27 @@ class ResidualBlock(nn.Module):
     conv - relu - conv - identity - relu
     """
     
-    def __init__(self, channels):
+    def __init__(self, input_channels, output_channels):
         super().__init__()
+        self.sub_stride = 1
         
-        input_channels = channels
-        output_channels = channels
-        self.conv1 = nn.Conv2d(input_channels, output_channels, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(input_channels, output_channels, kernel_size=3, padding=1)
+        # layers for subsampling
+        if input_channels != output_channels: 
+            self.sub_stride = 2
+            self.max = nn.MaxPool2d(1, stride=self.sub_stride)
+        self.conv1 = nn.Conv2d(input_channels, output_channels, kernel_size=3, stride=self.sub_stride, padding=1)
+        self.conv2 = nn.Conv2d(output_channels, output_channels, kernel_size=3, padding=1)
+        
         
     def forward(self, x):
         residual = self.conv2(F.relu(self.conv1(x)))
+        
+        # resize x by zero padding and downsampling via maxpoling to match dimensions of feature maps
+        if residual.shape[1] != x.shape[1]:
+            pad = int((residual.shape[1] - x.shape[1])/2)
+            x = F.pad(x, (0, 0, 0, 0, pad, pad, 0, 0))
+            x = self.max(x)
+            
         out = residual + x
         out = F.relu(out)
         return out
@@ -66,12 +72,12 @@ class ResidualBlocks(nn.Module):
     Sequence of n Residual Blocks of same structure.
     
     """
-    def __init__(self, channel, n):
+    def __init__(self, input_channels, output_channels, n):
         super().__init__()
-        self.n = n
-        blocks = []
-        for _ in range(n):
-            blocks.append(ResidualBlock(channel))
+        #self.n = n
+        blocks = [ResidualBlock(input_channels, output_channels)]
+        for _ in range(n-1):
+            blocks.append(ResidualBlock(output_channels, output_channels))
         self.blocks = nn.Sequential(*blocks)
         
     def forward(self, x):
