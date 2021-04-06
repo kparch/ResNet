517b745e389a99ff4e765823d55e70a99ad3ba3c
KasiaPar <kathrinp@online.de>
2021-04-06 14:22:19 +0200

    weight initialization and batchnorm

--- ResNet.py
+++ ResNet.py
@@ -16,6 +16,7 @@ class ResNet(nn.Module):
         super().__init__()
         self.n = n
         self.initial_layer = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.bn = nn.BatchNorm2d(16)
         nn.init.kaiming_normal_(self.initial_layer.weight)
         self.layerBlock1 = ResidualBlocks(16, 16, n)
         self.layerBlock2 = ResidualBlocks(16, 32, n)
@@ -23,10 +24,13 @@ class ResNet(nn.Module):
         self.avgpool = nn.AvgPool2d(kernel_size=8)
         self.fc = nn.Linear(64, 10)
         nn.init.kaiming_normal_(self.fc.weight)
+        nn.init.constant_(self.fc.bias, 0)
         
     def forward(self, x):
         initial_layer = self.initial_layer(x)
-        resblocks_1 = self.layerBlock1(initial_layer)
+        initial_bn = self.bn(initial_layer)
+        initial_relu = F.relu(initial_bn)
+        resblocks_1 = self.layerBlock1(initial_relu)
         resblocks_2 = self.layerBlock2(resblocks_1)
         resblocks_3 = self.layerBlock3(resblocks_2)
         avg_pool = self.avgpool(resblocks_3)
@@ -52,22 +56,32 @@ class ResidualBlock(nn.Module):
             self.sub_stride = 2
             self.max = nn.MaxPool2d(1, stride=self.sub_stride)
         self.conv1 = nn.Conv2d(input_channels, output_channels, kernel_size=3, stride=self.sub_stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(output_channels)
         self.conv2 = nn.Conv2d(output_channels, output_channels, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(output_channels)
         
         nn.init.kaiming_normal_(self.conv1.weight, a=0, mode='fan_in', nonlinearity='relu')
         nn.init.kaiming_normal_(self.conv2.weight, a=0, mode='fan_in', nonlinearity='relu')
+        nn.init.constant_(self.bn1.weight, 1)
+        nn.init.constant_(self.bn1.bias, 0)
+        nn.init.constant_(self.bn2.weight, 1)
+        nn.init.constant_(self.bn2.bias, 0)
         
         
     def forward(self, x):
-        residual = self.conv2(F.relu(self.conv1(x)))
+        conv1 = self.conv1(x)
+        bn1 = self.bn2(conv1)
+        relu1 = F.relu(bn1)
+        residual = self.conv2(relu1)
+        bn2 = self.bn2(residual)
         
         # resize x by zero padding and downsampling via maxpoling to match dimensions of feature maps
-        if residual.shape[1] != x.shape[1]:
-            pad = int((residual.shape[1] - x.shape[1])/2)
+        if bn2.shape[1] != x.shape[1]:
+            pad = int((bn2.shape[1] - x.shape[1])/2)
             x = F.pad(x, (0, 0, 0, 0, pad, pad, 0, 0))
             x = self.max(x)
             
-        out = residual + x
+        out = bn2 + x
         out = F.relu(out)
         return out
     
